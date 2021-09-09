46abf7e7319faa34c45fefe893c6c4e94b44f2d3
Aaron Nguyen <aaronnguyen.me@outlook.com>
2021-09-09 15:13:32 -0400

    Add social links

--- portfolio-react/src/components/Nav.js
+++ portfolio-react/src/components/Nav.js
@@ -1,12 +1,45 @@
-import { HStack, Link } from '@chakra-ui/layout';
+import { Link, Icon, HStack, Flex } from '@chakra-ui/react';
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export const Nav = () => {
   return (
-    <HStack justify="end" spacing="2rem" marginTop="2rem">
-      <Link fontSize="xl">Projects</Link>
-      <Link fontSize="xl">Resume</Link>
-      <Link fontSize="xl">Experience</Link>
-      <Link fontSize="xl">Contact</Link>
-    </HStack>
+    <Flex marginTop="2rem" justify="space-between" align="flex-end">
+      <HStack
+        spacing="1rem"
+        visibility={['collapse', 'collapse', 'visible', 'visible']}
+      >
+        <Link fontSize="xl" textDecoration="none">
+          Projects
+        </Link>
+        <Link fontSize="xl">Resume</Link>
+        <Link fontSize="xl">Experience</Link>
+      </HStack>
+      <HStack spacing="1rem">
+        <Link
+          href="mailto:me@aaronnguyen.dev"
+          isExternal
+          fontSize="2xl"
+          _hover={{ color: 'yellow.200' }}
+        >
+          <Icon as={FaEnvelope} />
+        </Link>
+        <Link
+          href="https://www.linkedin.com/in/aaron-nguyen-672b46204"
+          isExternal
+          fontSize="2xl"
+          _hover={{ color: 'yellow.200' }}
+        >
+          <Icon as={FaLinkedin} />
+        </Link>
+        <Link
+          href="https://github.com/aaronthangnguyen"
+          isExternal
+          fontSize="2xl"
+          _hover={{ color: 'yellow.200' }}
+        >
+          <Icon as={FaGithub} />
+        </Link>
+      </HStack>
+    </Flex>
   );
 };
