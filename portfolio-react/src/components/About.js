cee32d04012030b6dc85b3e256f73faa15a1ed2b
Aaron Nguyen <aaronnguyen.me@outlook.com>
2021-09-09 15:13:48 -0400

    Add responsive design

--- portfolio-react/src/components/About.js
+++ portfolio-react/src/components/About.js
@@ -1,29 +1,59 @@
-import { ButtonGroup, Flex, Heading, Text, Button } from '@chakra-ui/react';
-import { ViewIcon, AttachmentIcon, AtSignIcon } from '@chakra-ui/icons';
+import { Stack, Flex, Heading, Text, Button, Center } from '@chakra-ui/react';
+import { FaProjectDiagram, FaPaperclip, FaFlask } from 'react-icons/fa';
 
 export const About = () => {
   return (
-    <Flex direction="column" align="center" marginTop="5rem">
-      <Heading as="h2" size="3xl" marginLeft="2rem">
-        Hello, I'm Aaron 🐝
-      </Heading>
-      <Text fontSize="2xl" marginRight="6rem" marginTop="0.5rem">
+    <Flex
+      direction="column"
+      alignItems="center"
+      marginTop={['3rem', '3rem', '5rem', '5rem']}
+    >
+      {/* Heading */}
+      <Center paddingLeft="1rem">
+        <Heading
+          as="h2"
+          size="3xl"
+          visibility={['collapse', 'collapse', 'visible', 'visible']}
+        >
+          Hello, I'm&nbsp;
+        </Heading>
+        <Heading as="h2" size="3xl">
+          Aaron 🐝
+        </Heading>
+      </Center>
+
+      {/* Subheading */}
+      <Text
+        fontSize="2xl"
+        marginRight="6rem"
+        marginTop="0.5rem"
+        visibility={['collapse', 'collapse', 'visible', 'visible']}
+      >
         I build web applications
       </Text>
-      <Text fontSize="2xl" marginLeft="6rem" marginTop="0rem">
+      <Text
+        fontSize="2xl"
+        marginLeft="6rem"
+        marginTop="0rem"
+        visibility={['collapse', 'collapse', 'visible', 'visible']}
+      >
         from front-end to back-end.
       </Text>
-      <ButtonGroup marginTop="1rem" spacing="1rem">
-        <Button size="lg" leftIcon={<ViewIcon />}>
+      <Stack
+        marginTop="1rem"
+        spacing="1rem"
+        direction={['column', 'column', 'row', 'row']}
+      >
+        <Button size="lg" leftIcon={<FaProjectDiagram />}>
           Projects
         </Button>
-        <Button size="lg" colorScheme="yellow" leftIcon={<AttachmentIcon />}>
+        <Button size="lg" colorScheme="yellow" leftIcon={<FaPaperclip />}>
           Resume
         </Button>
-        <Button size="lg" leftIcon={<AtSignIcon />}>
+        <Button size="lg" leftIcon={<FaFlask />}>
           Experience
         </Button>
-      </ButtonGroup>
+      </Stack>
     </Flex>
   );
 };
