5aa73b900be4466cd47bebf1236f78ff44fbe1e6
Aaron Nguyen <aaronnguyen.me@outlook.com>
2021-09-11 14:17:59 -0400

    Add Footer

---
+++ portfolio-react/src/components/Footer.js
@@ -0,0 +1,15 @@
+import { Center, Divider, Heading, Text } from '@chakra-ui/react';
+
+export const Footer = () => {
+  return (
+    <Center marginBottom="0.5rem" flexDirection="column" textAlign="center">
+      <Divider />
+      <Heading as={Text} marginTop="0.5rem" size="md">
+        Made with ❤️ by Aaron Nguyen
+      </Heading>
+      <Heading as={Text} size="sm">
+        Powered by React, Chakra UI, and GitHub Pages
+      </Heading>
+    </Center>
+  );
+};
