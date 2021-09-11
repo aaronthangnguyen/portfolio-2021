e76630ce59d5a112f4fee4e8463eca6ec9bb8590
Aaron Nguyen <aaronnguyen.me@outlook.com>
2021-09-11 14:18:54 -0400

    Convert flexbox to grid system, add new components

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import { About } from './components/About';
-
 import '@fontsource/ubuntu';
-
-import { ChakraProvider, Container } from '@chakra-ui/react';
+import { ChakraProvider, Container, Grid, GridItem } from '@chakra-ui/react';
 import theme from './theme';
 import { Nav } from './components/Nav';
+import { Hero } from './components/Hero';
+import { Projects } from './components/Projects';
+import { Experience } from './components/Experience';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Container maxW="container.lg">
-        <Nav />
-        <About />
+        <Grid
+          height="100vh"
+          templateRows="min-content min-content auto min-content"
+          templateColumns="repeat(2, 1fr)"
+          gap="2rem 2rem"
+        >
+          <GridItem colSpan={2}>
+            <Nav />
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Hero />
+          </GridItem>
+          <GridItem colSpan={[2, 2, 1, 1]}>
+            <Projects />
+          </GridItem>
+          <GridItem colSpan={[2, 2, 1, 1]}>
+            <Experience />
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Footer />
+          </GridItem>
+        </Grid>
       </Container>
     </ChakraProvider>
   );
