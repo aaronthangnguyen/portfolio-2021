00e61862f21767e0badf0c30066faa6d7da1891a
Aaron Nguyen <aaronnguyen.me@outlook.com>
2021-09-12 10:29:13 -0400

    JK! Increase margin top

--- portfolio-react/src/components/Footer.js
+++ portfolio-react/src/components/Footer.js
@@ -2,9 +2,9 @@ import { Center, Divider, Heading, Text } from '@chakra-ui/react';
 
 export const Footer = () => {
   return (
-    <Center marginBottom="0.5rem" flexDirection="column" textAlign="center">
+    <Center marginBottom="1rem" flexDirection="column" textAlign="center">
       <Divider />
-      <Heading as={Text} marginTop="1rem" size="md">
+      <Heading as={Text} marginTop="0.5rem" size="md">
         Made with ❤️ by Aaron Nguyen
       </Heading>
       <Heading as={Text} size="sm">
